8b8c55010074653cba624b5cc91fd032959184b5
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-18 17:59:46 +0530

    Added type to locations

--- src/App.js
+++ src/App.js
@@ -6,44 +6,53 @@ import './App.css';
 class App extends Component {
 	state = {
 		map:'',
-		places:[
+		markers:[],
+		locations:[
           {
             name:'Burger farm',
+            type:'Restaurant',
             lat:26.9063,
             lng:75.7964
           },
           {
             name:'Cafe RJ14',
+            type:'Restaurant',
             lat:26.9039,
             lng:75.7947
           },
           {
             name: 'Home Cafe by Mr.Beans',
+            type:'Restaurant',
             lat:26.9050,
             lng:75.7941
           },
             {
             name:'Kanha Restaurant Jaipur',
+            type:'Restaurant',
             lat:26.9123,
             lng:75.8006
           },
           {
             name:'Hawa Mahal',
+            type:'Tourist Place',
             lat:26.9239,
             lng:75.8267
           },
           {
             name:'MGF Metropolitan Mall',
+            type:'Shopping Mall',
             lat:26.9027,
             lng:75.7937
           },
           {
             name:'INOX Jaipur - C Scheme / Crystal Palm',
+            type:'Shopping Mall',
             lat:26.9034,
             lng:75.7925
           },
           {
           	name:'Cafe F-32',
+          	type:'Restaurant',
           	lat:26.9075,
           	lng:75.7956
           }
@@ -63,6 +72,14 @@ class App extends Component {
             zoom: 14
       });
       this.setState({map:map})
+
+      this.state.locations.forEach(location => {
+      	const marker = new.google.maps.Marker({
+      		position: {lat: location.location.lat, lng:location.location.lng}
+      		map:map
+      		title:location.name
+      	})
+      })
   }
   render() {
     return (
